9fee201b793ef1bd817f9639e3bdae75524eb6fb
kawanaki8561-cyber <kawanaki8561@gmail.com>
2026-07-20 15:41:37 +0900

    Update:N+1問題検証_片付け

--- app/controllers/tests_controller.rb
+++
@@ -1,6 +0,0 @@
-class TestsController < ApplicationController
-  def index
-    # N+1を発生させるため、事前読み込み（includesなど）をせずに全件取得します
-    @posts = Post.all
-  end
-end
--- app/models/post.rb
+++
@@ -1,3 +0,0 @@
-class Post < ApplicationRecord
-    has_many :comments # これを追記して保存してください
-end
