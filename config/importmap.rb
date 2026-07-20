0b069ec0a6f0833f0cd7942e575fd3f40295aefc
kawanaki8561-cyber <kawanaki8561@gmail.com>
2026-07-20 17:09:53 +0900

    Fix:chart.jsモジュール読み込みエラー対応

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,7 +3,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin 'application'
-pin '@hotwired/turbo-rails', to: 'turbo.min.js'
+pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @8.0.23
 pin '@hotwired/stimulus', to: 'stimulus.min.js'
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
@@ -11,3 +11,5 @@ pin "papaparse" # @5.5.4
 pin "simple-statistics" # @7.9.3
 pin "chart.js" # @4.5.1
 pin "@kurkle/color", to: "@kurkle--color.js" # @0.3.4 chart.js が内部で使っている依存ライブラリ
+pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.23
+pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @8.1.300
