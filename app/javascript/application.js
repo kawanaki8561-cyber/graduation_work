01bd2691bc9dd7c6d224c08dc54081c82c0d9018
kawanaki8561-cyber <kawanaki8561@gmail.com>
2026-07-20 17:41:37 +0900

    Fix:不要な残存設定の削除

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "controllers"
+// import "controllers"
 
 // Issue#7_各ライブラリのインポート
 import Papa from "papaparse";
 // ※ simple-statistics はES6向けに名前付きエクスポートのみを提供しているため、以下のようにインポートします
 import * as ss from "simple-statistics"; 
-// ※ Chart.jsは通常、autoを付けてインポートするのが便利です
-import Chart from "chart.js/auto";
+// ↓ 修正箇所: "chart.js" からモジュールを読み込み、手動で一括登録する形式に変更します
+import { Chart, registerables } from "chart.js";
+Chart.register(...registerables);
 
 // ブラウザでの読み込みテスト
 document.addEventListener("DOMContentLoaded", ()=> {
