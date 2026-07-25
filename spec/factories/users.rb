0b3e92aef5500d934cdd965a78e92df4311121b2
kawanaki8561-cyber <kawanaki8561@gmail.com>
2026-07-25 21:24:59 +0900

    Add:userテストモデル作製機能追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
+    sequence(:username) { |n| "user_name#{n}" }
+    sequence(:email){ |n| "user#{n}@example.com" }
+    passworld{ "password123" }
     
   end
 end
