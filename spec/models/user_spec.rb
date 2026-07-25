02b61d4a96be0de2a75683b4be76edf8563c7ddf
kawanaki8561-cyber <kawanaki8561@gmail.com>
2026-07-25 21:41:00 +0900

    Add:userモデルのテスト内容追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションのテスト' do
+    # 一意性のテストなどでエラーを防ぐため、事前に有効なデータを作成しておく
+    subject { FactoryBot.build(:user) }
+
+    context '必須項目の検証' do
+      it { is_expected.to validate_presence_of(:username) } # usernameの必須テスト
+      it { is_expected.to validate_presence_of(:email) }
+      it { is_expected.to validate_presence_of(:password) }
+    end
+
+    context '一意性の検証' do
+      it { is_expected.to validate_uniqueness_of(:username) } # usernameの重複テスト
+      it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+    end
+  end
 end
