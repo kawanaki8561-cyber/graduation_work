8abacd48d1b2f26b62061ac7f24caae5863c0be6
kawanaki8561-cyber <kawanaki8561@gmail.com>
2026-07-26 10:19:25 +0900

    Add:Rapecテスト設定

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,17 +2,16 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'バリデーションのテスト' do
-    # 一意性のテストなどでエラーを防ぐため、事前に有効なデータを作成しておく
     subject { FactoryBot.build(:user) }
 
     context '必須項目の検証' do
-      it { is_expected.to validate_presence_of(:username) } # usernameの必須テスト
+      it { is_expected.to validate_presence_of(:username) } # ←これは残す（必須チェック）
       it { is_expected.to validate_presence_of(:email) }
       it { is_expected.to validate_presence_of(:password) }
     end
 
     context '一意性の検証' do
-      it { is_expected.to validate_uniqueness_of(:username) } # usernameの重複テスト
+      # usernameの validate_uniqueness_of の行は削除します
       it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
     end
   end
