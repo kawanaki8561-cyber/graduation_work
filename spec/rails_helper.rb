980bfc11587677d71ec08277f9a67c2a99908975
kawanaki8561-cyber <kawanaki8561@gmail.com>
2026-08-01 13:43:49 +0900

    Add:ヘッドレスブラウザの設定

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -44,6 +44,11 @@ RSpec.configure do |config|
     Rails.root.join('spec/fixtures')
   ]
 
+  # System Spec実行時のデフォルトドライバをヘッドレスChromeに設定
+  config.before(:each, type: :system) do
+    driven_by(:selenium, using: :headless_chrome)
+  end
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
