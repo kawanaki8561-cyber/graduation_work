625423e62ef9adb787b647aefabf8f7a6b978574
kawanaki8561-cyber <kawanaki8561@gmail.com>
2026-08-20 22:25:08 +0900

    add:データベースの準備

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_07_24_093525) do
+ActiveRecord::Schema[7.1].define(version: 2026_08_20_113114) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "username"
     t.string "email", default: "", null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_07_24_093525) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "guest", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
